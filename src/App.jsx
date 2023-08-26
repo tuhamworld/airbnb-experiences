e4a5f6078e029c7e7bfe8ffe9415cd3d26c03143
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-08-26 14:22:51 +0100

    cls

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,31 @@ import './App.css'
 
 function App() {
   const newDatas = data.map(singleData => {
-
     return <Card
-      
-        card = {singleData.id}
-        img={singleData.coverImg}
-        rating={singleData.stats.rating}
-        reviewCount={singleData.stats.reviewCount}
-        location={singleData.location}
-        title={singleData.title}
-        price={singleData.price}
-        openSpots = {singleData.openSpots}
-      
+      key={singleData.id}
+      singleData = {singleData}      
       />
-    
-  
   })
-
   return (
     <>
       <Navbar />
-      <Hero />
+    <Hero />
       <section className="card-container container"> 
       {newDatas}
-
       </section>
-
-      
     </>
-
   )
 }
 
 export default App
+
+
+// Previously while destructuring props
+// card = {singleData.id}
+// img={singleData.coverImg}
+// rating={singleData.stats.rating}
+// reviewCount={singleData.stats.reviewCount}
+// location={singleData.location}
+// title={singleData.title}
+// price={singleData.price}
+// openSpots = {singleData.openSpots}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,38 +2,25 @@ import React from "react"
 import star from '../assets/Star.png'
 
 // Using Props for displaying Card data
-export default function Card({ img, rating, reviewCount, location, title, price, openSpots }) {
-
+export default function Card(props) {
     let badgeContent
-
-    if (openSpots === 0) {
+    if (props.openSpots === 0) {
         badgeContent = "SOLD OUT"
-    } else if (openSpots > 0) {
-        badgeContent = location
-    } else {
-        badgeContent = ""
+    } else if (props.location === 'Online') {
+        badgeContent = "Online"
     }
-
     return (
-        <div className="card">
-            
-            {<div className="card-badge">{badgeContent}</div>}
-
-                <img src={`./images/${img}`} className="card-image"/>
-
+        <div className="card">       
+            {badgeContent && <div className="card-badge">{badgeContent}</div>}
+                <img src={`./images/${props.singleData.coverImg}`} className="card-image"/>
                 <div className="card-stats">
                     <img src={star} className="card-star" />
-                    <span className="card-review">{rating}</span>
-                    <span className="gray">({reviewCount}) • </span>
-                    <span> {location}</span>
+                    <span className="card-review">{props.singleData.stats.rating}</span>
+                    <span className="gray">({props.singleData.stats.reviewCount}) • </span>
+                    <span> {props.singleData.location}</span>
                 </div>
-
-                
-                    <p>{title}</p>
-                    <p><span className="bold">From ${price}</span> / person</p>
-
-                
-
+                    <p>{props.singleData.title}</p>
+                    <p><span className="bold">From ${props.singleData.price}</span> / person</p>
                 </div>      
         
     )
